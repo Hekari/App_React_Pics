c83c064782e2d784b9501a31e6f9b74465ac15ae
Sxymik <49282273+Hekari@users.noreply.github.com>
2022-06-01 23:09:52 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID bv5W3rAqdE-1y6WrwvPIR_aNRGdi443SSSSHwuBoqWo",
+    Authorization: "Client-ID supersecretkey",
   },
 });
